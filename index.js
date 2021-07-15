37859002cc97fd12a445435819fd84d95b693c0f
Jonathan Mann <jdmann84@gmail.com>
2021-07-14 19:08:47 -0700

    installed chalk and added better color to console.log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import packages - added by Jonathan 07-13-2021 @ 7PM Pacific Time
 const express = require('express');
+const chalk = require('chalk');
 const server = express();
-server.listen(3000, () => 'listening on port 3000');
+server.listen(3001, () => console.log(chalk.red('listening on port 3001')));
